f21464f87d4b93cbed9e29dc0bcb5cabce329427
reasonmee <yunable07@naver.com>
2025-05-09 10:08:11 +0000

    입력예시, 레이아웃 연습

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,37 @@ st.info(
 )
 
 # 페이지 구조용 제목 출력
-st.title("내 이름은 노승우 or 이유나")
-st.header("힌트는 여자")
-st.subheader("정답은 이유나")
+st.title("지금부터")
+st.header("귀여운 고양이가 등장합니당")
+st.subheader("출처는 구글")
 
 
 
-st.warning("⚠️ 못 맞췄으면 나가라.")
+st.warning("⚠️ 귀여움 주의")
+st.image("https://cdn.animaltoc.com/news/photo/202410/1409_6515_3838.jpg")
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGIxeTV0ZW9nMnFxdXppZm1kMXpudjc0aWozYmV1MHl6dHkyZ2dxaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/5i7umUqAOYYEw/giphy.gif")
+
+
+# st.columns(n): 화면을 n개의 수직 열로 나눕니다
+col1, col2 = st.columns(2)  # 2개의 열 생성
+
+with col1:
+    st.write("고양이")  # 첫 번째 열에 내용 작성
+with col2:
+    st.write("냥냥냥")  # 두 번째 열에 내용 작성
+
+# st.expander("제목"): 내용을 접었다 펼 수 있는 컨테이너입니다
+with st.expander("ℹ️ 강아지는?"):
+    st.write("멍멍멍")
+
+choice = st.text_input("고양이와 강아지 중 더 귀여운 것은?")
+
+if choice == "고양이":
+    st.success(choice+"가 더 귀엽긴해요")
+else :
+    st.error("응아니야")
+
+
+# 여러 옵션 중 하나 선택
+food = st.radio("먹고 싶은 음식을 선택하세요", ["엽떡", "순두부열라면", "뿌링클"])
+st.write("오늘 저녁 메뉴 :"+ food)
